c4a79ea5a490d11ecab0a6b5b25319247e8420e3
Festo1 <festoeducation@gmail.com>
2023-09-24 16:35:48 +0300

    105-radix_sort.c

---
+++ 105-radix_sort.c
@@ -0,0 +1,91 @@
+#include "sort.h"
+
+int get_max(int *array, int size);
+void radix_counting_sort(int *array, size_t size, int sig, int *buff);
+void radix_sort(int *array, size_t size);
+
+/**
+ * get_max - Get the maximum value in an array of integers.
+ * @array: An array of integers.
+ * @size: The size of the array.
+ *
+ * Return: The maximum integer in the array.
+ */
+int get_max(int *array, int size)
+{
+	int max, i;
+
+	/* Initialize max as first element */
+	for (max = array[0], i = 1; i < size; i++)
+	{
+		/* Check if current element is greater than max */
+		if (array[i] > max)
+			max = array[i];
+	}
+
+	return (max);
+}
+
+/**
+ * radix_counting_sort - Sort the significant digits of an array of integers
+ * in ascending order using the counting sort algorithm.
+ * @array: An array of integers.
+ * @size: The size of the array.
+ * @sig: The significant digit to sort on.
+ * @buff: A buffer to store the sorted array.
+ */
+void radix_counting_sort(int *array, size_t size, int sig, int *buff)
+{
+	int count[10] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+	size_t i;
+
+	/* Count occurrences of each digit */
+	for (i = 0; i < size; i++)
+		count[(array[i] / sig) % 10] += 1;
+
+	/* Calculate position of each digit */
+	for (i = 0; i < 10; i++)
+		count[i] += count[i - 1];
+
+	/* Build output array */
+	for (i = size - 1; (int)i >= 0; i--)
+	{
+		buff[count[(array[i] / sig) % 10] - 1] = array[i];
+		count[(array[i] / sig) % 10] -= 1;
+	}
+	/* Copy to original array */
+	for (i = 0; i < size; i++)
+		array[i] = buff[i];
+}
+
+/**
+ * radix_sort - Sort an array of integers in ascending
+ * order using the radix sort algorithm.
+ * @array: An array of integers.
+ * @size: The size of the array.
+ *
+ * Description: Implements the LSD radix sort algorithm. Prints
+ * the array after each significant digit increase.
+ */
+void radix_sort(int *array, size_t size)
+{
+	int max, sig, *buff;
+
+	/* Invalid input */
+	if (array == NULL || size < 2)
+		return;
+	/* Allocate temp buffer */
+	buff = malloc(sizeof(int) * size);
+	if (buff == NULL)
+		return;
+
+	max = get_max(array, size);
+	 /* Sort by each digit place */
+	for (sig = 1; max / sig > 0; sig *= 10)
+	{
+		radix_counting_sort(array, size, sig, buff);
+		print_array(array, size);
+	}
+
+	free(buff);
+}
